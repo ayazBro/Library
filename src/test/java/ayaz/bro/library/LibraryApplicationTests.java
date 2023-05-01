ccdbc2f9424cc59d03bfc80f4795ae109799f0e0
Ilya Tsaregorodtsev <zarik02789@gmail.com>
2023-05-01 16:51:02 +0300

    firstTest

--- src/test/java/ayaz/bro/library/LibraryApplicationTests.java
+++ src/test/java/ayaz/bro/library/LibraryApplicationTests.java
@@ -1,13 +1,32 @@
 package ayaz.bro.library;
 
+import ayaz.bro.library.controllers.ClientController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.core.StringContains.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class LibraryApplicationTests {
-
+	@Autowired
+	MockMvc mockMvc;
+	@Autowired
+	ClientController clientController;
 	@Test
-	void contextLoads() {
+	void basePage() throws Exception{
+		this.mockMvc.perform(get("/"))
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(content().string(containsString("Welcome to our library")));
+
 	}
 
 }
